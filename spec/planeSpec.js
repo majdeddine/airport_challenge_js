633ab5b1eb0756755531fcf668b34791850481be
majdeddine <majda-jambouri@hotmail.fr>
2017-11-21 12:59:20 +0000

    change plane status when landing or takingoff

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -1,1 +1,22 @@
+describe("Plane",function(){
+  it("have a #status",function(){
+    var plane = new Plane()
+    expect(plane.staus).toBeFalsy()
+  })
 
+  it("change #status when landing", function(){
+  var plane = new Plane()
+  var airport = new Airport()
+  airport.land(plane)
+  expect(plane.status).toBeTruthy()
+})
+
+it("change #status after takeOff",function(){
+  var plane = new Plane()
+  var airport = new Airport()
+  airport.land(plane)
+  airport.takeOff(plane)
+  expect(plane.status).toBeFalsy()
+})
+
+});
--- src/airport.js
+++ src/airport.js
@@ -4,8 +4,10 @@ function Airport(){
 
 Airport.prototype.land = function(plane){
   this.planes.push(plane)
+  plane.status = true
 }
 
 Airport.prototype.takeOff = function(plane){
   this.planes.pop()
+  plane.status = false
 }
