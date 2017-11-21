9140b25fe6ed80c988c221e0da84304790282951
majdeddine <majda-jambouri@hotmail.fr>
2017-11-21 12:53:26 +0000

    add takeOff to the airport

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -4,13 +4,22 @@ describe("Airport", function() {
   beforeEach(function(){
     airport = new Airport();
   })
+
   it("have a planes array", function(){
     var plane = jasmine.createSpyObj('plane',['status'])
     expect(airport.planes).toEqual(jasmine.any(Array))
   })
+
   it("#land a plane",function(){
     var plane = jasmine.createSpyObj('plane',['status'])
       airport.land(plane)
       expect(airport.planes).toContain(plane)
     })
+
+    it ("#takeOff a landed plane", function(){
+    var plane = jasmine.createSpyObj('plane',['status'])
+    airport.land(plane)
+    airport.takeOff(plane)
+    expect(airport.planes.length).toEqual(0)
+  })
 });
