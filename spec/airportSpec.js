a6a9eb288792521539d742a21f91c73cf1e3c91f
majdeddine <majda-jambouri@hotmail.fr>
2017-11-21 12:49:50 +0000

    add plane as a spy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,15 +1,15 @@
 describe("Airport", function() {
   var airport;
-  var plane;
+
   beforeEach(function(){
     airport = new Airport();
   })
   it("have a planes array", function(){
-
+    var plane = jasmine.createSpyObj('plane',['status'])
     expect(airport.planes).toEqual(jasmine.any(Array))
   })
   it("#land a plane",function(){
-
+    var plane = jasmine.createSpyObj('plane',['status'])
       airport.land(plane)
       expect(airport.planes).toContain(plane)
     })
